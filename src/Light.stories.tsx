0a4048fc871b047aaafb9a1c5958ecc4f7a2a020
SoftEngMatthew <mateus.fonto@gmail.com>
2023-07-08 16:56:09 -0300

    initial-stage

---
+++ src/Light.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Light from "./Light";
+
+const meta: Meta<typeof Light> = {
+  component: Light,
+  title: "Light",
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Base: Story = {
+  args: {},
+};
+
+export const Example: Story = {
+  args: {},
+};
+
+// 11:49
